e04da6ef9d40f97722f560a0c3c1fb176010bec1
Luiz Branco <artfikan@gmail.com>
2012-04-26 20:18:37 -0300

    Added vi opened swap files to ignore list

--- lib/action.rb
+++ lib/action.rb
@@ -5,8 +5,18 @@ module RubyQuest
     attr_accessor :location
 
     def go location
-      self.location = location
-      Output.action "You have reached the #{location.name}"      
+      if on_map?(location)
+        self.location = location
+        Output.action "You have reached the #{location.name}"      
+      else
+        Output.action "You don't have #{location.name} on your map. Type 'map' to see the available locations"
+      end
+    end
+
+    private
+
+    def on_map?(location)
+      {:available => ['forest'] }.include?(location)      
     end
 
   end
--- spec/lib/action_spec.rb
+++ spec/lib/action_spec.rb
@@ -16,21 +16,44 @@ module RubyQuest
 
     let(:actor) { Dummy.new }
 
-    context "when going to a location" do
+    context "when going to a new location" do
 
-      let(:forest) { double('location', :name => 'forest')}
+      let(:forest) { double('new_location', :name => 'forest')}
 
-      it "changes the location to new one" do
-        actor.go forest
-        actor.location.should eq forest
+      context "and the location is on the map" do
+
+        before do
+          actor.stub(:on_map?).and_return(true)
+        end
+
+        it "travels to this location" do
+          actor.go forest
+          actor.location.should eq forest
+        end
+
+        it "informs that the location has been reached" do
+          Output.should_receive(:action).with('You have reached the forest')
+          actor.go forest
+        end
+
       end
 
-      it "informs that the location has been reached" do
-        Output.should_receive(:action).with('You have reached the forest')
-        actor.go forest
-      end
+      context "and the location is not on the map" do
 
-      it "informs that this location isn't available"
+        let(:mountain) { double('old_location', :name => 'mountain')}
+
+        it "doesn't travel to this location" do
+          actor.location = mountain
+          actor.go forest
+          actor.location.should eq mountain
+        end
+
+        it "informs that this location is unavailable" do
+          Output.should_receive(:action).with("You don't have forest on your map. Type 'map' to see the available locations")
+          actor.go forest
+        end
+
+      end
 
     end
 
