23e5b3d705d4577e5826fcfa237c9b3cbc37a0de
Luiz Branco <artfikan@gmail.com>
2012-04-29 13:42:53 -0300

    Refactores Commands tests

--- spec/lib/ruby_quest/command_spec.rb
+++ spec/lib/ruby_quest/command_spec.rb
@@ -5,22 +5,22 @@ module RubyQuest
 
   describe Command do
 
+    let(:output) { output = double('output') }
+    let(:command) { command = Command.new(output) }
+
     context "when going to a new location" do
 
       let(:forest) { double('new_location', :name => 'forest')}
+      let(:map) { double('map')}
 
       it "informs that the location has been reached" do
-        output = double('output')
-        command = Command.new(output)
-        map = double("map", :travel => true)
+        map.should_receive(:travel).and_return(true)
         output.should_receive(:react).with("You have reached the forest")
         command.go forest, map
       end
 
       it "informs that this location is unavailable" do
-        output = double('output')
-        command = Command.new(output)
-        map = double("map", :travel => false)
+        map.should_receive(:travel).and_return(false)
         output.should_receive(:react).with("Forest is not on your map. Type 'map' to list all avaiable locations")
         command.go forest, map
       end
