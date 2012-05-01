a7ea77e4fd06b39daee0eb8bed38c256876dfd94
Luiz Branco <artfikan@gmail.com>
2012-04-30 22:49:38 -0300

    Added remaning specs for CLI

--- spec/lib/ruby_quest/cli_spec.rb
+++ spec/lib/ruby_quest/cli_spec.rb
@@ -5,53 +5,78 @@ module RubyQuest
 
   describe CLI do
 
-    before do
-      @input = double.as_null_object
-      @output = double.as_null_object
-      @cli = CLI.new @input, @output
-      hero = double('hero', :name => 'Tas')
-      Hero.stub(:new).and_return(false, hero) # Runs the loop twice 
-      @cli.stub(:console)
-    end
+    context "when starting a game" do
 
-    it "shows a greeting message" do
-      @output.should_receive(:announce).with("Greeting stranger, welcome to RubyQuest! May I have your name?")
-      @cli.start!
-    end
+      before do
+        @input = double.as_null_object
+        @output = double.as_null_object
+        @cli = CLI.new @input, @output
+        hero = double('hero', :name => 'Tas')
+        Hero.stub(:new).and_return(false, hero) # Runs the loop twice 
+        @cli.stub(:console)
+      end
 
-    it "shows a prompt message for choosing a hero and for giving commands" do
-      @input.should_receive(:readline).with("RubyQuest ~> ").twice
-      @cli.start!
-    end
-
-    context "when an Hero name is provided" do
-      it "creates a Hero" do
-        @input.stub(:readline).and_return('Caramon')
-        Hero.should_receive(:new).with('Caramon')
+      it "shows a greeting message" do
+        @output.should_receive(:announce).with("Greeting stranger, welcome to RubyQuest! May I have your name?")
         @cli.start!
       end
 
-      it "greetings the Hero" do
-        @output.should_receive(:announce).with("Nice to meet you Tas")
+      it "shows a prompt message for choosing a hero and for giving commands" do
+        @input.should_receive(:readline).with("RubyQuest ~> ").twice
         @cli.start!
+      end
+
+      context "when an Hero name is provided" do
+        it "creates a Hero" do
+          @input.stub(:readline).and_return('Caramon')
+          Hero.should_receive(:new).with('Caramon')
+          @cli.start!
+        end
+
+        it "greetings the Hero" do
+          @output.should_receive(:announce).with("Nice to meet you Tas")
+          @cli.start!
+        end
+      end
+
+      context "when an empty Hero name is passed" do
+        it "complains about a empty Hero name" do
+          @output.should_receive(:announce).with("C'mon give me your name!")
+          @cli.start!
+        end
       end
     end
 
-    context "when an empty Hero name is passed" do
-      it "complains about a empty Hero name" do
-        @output.should_receive(:announce).with("C'mon give me your name!")
+    context "when receives commands" do
+
+      before do
+        @input = double.as_null_object
+        @output = double.as_null_object
+        Command.stub(:new)
+        @cli = CLI.new @input, @output
+        @cli.stub(:set_user)
+        @input.stub(:readline).and_return('go to castle', 'exit') # Runs the loop twice
+      end
+
+      it "loads the console" do
+        @cli.should_receive(:console)
         @cli.start!
       end
-      it "doesn't load the console until a name is given"
+
+      it "saves the command on the history" do
+        @cli.start!
+        Readline::HISTORY.should include 'go to castle'
+      end
+
+      it "invoks the command" do
+        hero = double('hero')
+        @cli.stub(:hero).and_return(hero)
+        @input.stub(:readline).and_return('go to castle', 'exit')
+        Command.should_receive(:new).with('go to castle', hero)
+        @cli.start!
+      end
+
     end
-
-    it "loads the console" do
-      @cli.should_receive(:console)
-      @cli.start!
-    end
-
-    it "created an endless loop"
-
 
   end
 end
